ef99cedf37497dccbbf70499cc8781e38330a4ab
Rekord <1324596506@qq.com>
2022-02-03 19:48:10 +0800

    feat:add getaddress.py

---
+++ getaddress.py
@@ -0,0 +1,15 @@
+from yiban import Yiban
+import json
+
+if __name__ == '__main__':
+    try:
+        # read data from file.
+        with open('address.json', encoding='utf-8') as f:
+            json_datas = json.load(f)
+
+        yiban = Yiban(json_datas['mobile'], json_datas['password'])
+        yiban.get_address(month=json_datas['month'], day=json_datas['day'])
+    except KeyError as e:
+        print("address.json file error:")
+    except Exception as e:
+        print(e)
