cb7979ac3d5b124e82b3d197bede108cf049b653
Rekord <1324596506@qq.com>
2022-02-05 21:20:01 +0800

    chore:modify the format of msg and add some comments

--- main.py
+++ main.py
@@ -11,25 +11,27 @@ import requests
 
 
 def main_handler(data=None, extend=None):
+    # load json datas
     with open('config.json', encoding='utf-8') as f:
         json_datas = json.load(f)['Forms']
-
     # print(json_datas)
+
     for data in json_datas:
         success_flag = False
         while success_flag == False:
             success_flag = True
-            msg = f"{time.strftime('%y-%m-%d',time.localtime(time.time()))} 易班打卡："
             nickname = data['UserInfo']['NickName']
+            msg = f"{time.strftime('%y-%m-%d',time.localtime(time.time()))} {nickname}-易班打卡："
             address_info = data['AddressInfo']
             try:
                 yiban = Yiban(data['UserInfo']['Mobile'], data['UserInfo']['Password'])
                 yiban.submit_task(address_info)
-                msg = f'{msg}{nickname} 打卡成功.'
+                msg = f'{msg}Success.'
+            # If an error occurs due to network problems, the program will continue to run
             except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError) as e:
                 success_flag = False
             except Exception as e:
-                msg = f'{msg}{nickname}: {e}'
+                msg = f'{msg}{e}'
             finally:
                 if success_flag == True:                
                     print(msg)
