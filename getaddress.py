380a350f65ba4c55b04707bd6598e85116bfbf71
Rekord <1324596506@qq.com>
2022-03-17 18:57:59 +0800

    refactor: refactor the location of address info

--- getaddress.py
+++ getaddress.py
@@ -4,12 +4,12 @@ import json
 if __name__ == '__main__':
     try:
         # read data from file.
-        with open('address.json', encoding='utf-8') as f:
-            json_datas = json.load(f)
+        with open('config.json', encoding='utf-8') as f:
+            json_datas = json.load(f)['address']
 
         yiban = Yiban(json_datas['mobile'], json_datas['password'])
         yiban.get_address(month=json_datas['month'], day=json_datas['day'])
     except KeyError as e:
-        print("address.json file error:")
+        print("config.json file error:")
     except Exception as e:
         print(e)
