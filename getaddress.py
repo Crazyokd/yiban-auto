d5d9aab9def629f545c14b42e3e51c43ce3b54c7
Rekord <1324596506@qq.com>
2022-08-31 11:53:40 +0800

    feat: update getaddress.py

--- getaddress.py
+++ getaddress.py
@@ -7,8 +7,9 @@ if __name__ == '__main__':
         with open('config.json', encoding='utf-8') as f:
             json_datas = json.load(f)['address']
 
-        yiban = Yiban(json_datas['mobile'], json_datas['password'])
-        yiban.get_address(month=json_datas['month'], day=json_datas['day'])
+        yiban = Yiban(json_datas['mobile'], json_datas['password'], '每日健康打卡')
+        yiban.get_address()
+        print(yiban.get_picture("9a6da1b5c2519032945d1048a60d75f9"))
     except KeyError as e:
         print("config.json file error:")
     except Exception as e:
